f6dc948f0a27b0cfca1933c48818de4533e18ed3
Fabrizio Giuseppe Pignatelli <141920930+FabrizioPignatelli02@users.noreply.github.com>
2023-10-27 15:21:25 +0200

    add change color nav bar and button

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener("scroll", () => {
+  const nav = document.getElementById("nav");
+  const getStartedButton = document.getElementById("getStarted");
+  if (window.scrollY > 350) {
+    nav.classList.add("scrolled");
+    nav.classList.remove("unscrolled");
+    getStartedButton.classList.add("buttonScrolled");
+    getStartedButton.classList.remove("buttonUnScrolled");
+  } else {
+    nav.classList.add("unscrolled");
+    nav.classList.remove("scrolled");
+    getStartedButton.classList.add("buttonUnScrolled");
+    getStartedButton.classList.remove("buttonScrolled");
+  }
+});
